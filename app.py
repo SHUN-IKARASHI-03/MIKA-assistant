507e9d7947db465b005325dd635ad576aeda916c
SHUN-IKARASHI-03 <ax00.syun@gmail.com>
2025-04-09 01:33:04 +0900

    Update app.py

--- app.py
+++ app.py
@@ -40,3 +40,7 @@ def slack_events():
             slack_client.chat_postMessage(channel=channel, text=answer)
 
     return "OK", 200
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
